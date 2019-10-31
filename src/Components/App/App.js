45081726b4935406ab2b276c5f3b6d460e55982c
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-10-30 19:37:33 -0600

    Core setup for redux and router, and wrapping Provider.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { fetchPopularCities } from '../../apiCalls';
+import { fetchPopularCities, fetchLattitudeLongtitude } from '../../apiCalls';
 import FormInput from '../../Components/FormInput/FormInput';
 import Weather from '../../Components/Weather/Weather';
 import './App.css';
@@ -7,6 +7,7 @@ import './App.css';
 class App extends Component {
   componentDidMount = async () => {
     fetchPopularCities();
+    fetchLattitudeLongtitude();
   };
 
   render() {
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -10,3 +10,16 @@ export const fetchPopularCities = async () => {
     console.log(weatherData);
   }
 };
+
+export const fetchLattitudeLongtitude = async () => {
+  const response = await fetch(
+    'https://www.metaweather.com/api/location/search/?lattlong=38,-100'
+  );
+
+  if (!response.ok) {
+    throw Error('Failed to get weather data');
+  } else {
+    const weatherData = await response.json();
+    console.log(weatherData);
+  }
+};
