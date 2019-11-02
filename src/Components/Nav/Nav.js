1b082acc3bfc3080b90315fa6d55ce234505bdce
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-11-01 21:09:19 -0600

    Setup initial usage of router.

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Nav extends Component {
   render() {
     return (
       <nav>
         <h2>Weather</h2>
-        <ul class='nav-links'>
-          <li>Favorites</li>
-          <li>Custom</li>
-          <li>Home</li>
+        <ul className='nav-links'>
+          <Link to='/favorites'>
+            <li>Favorites</li>
+          </Link>
+          <Link to='/customlist'>
+            <li>Custom</li>
+          </Link>
+          <Link to='/'>
+            <li>Home</li>
+          </Link>
         </ul>
       </nav>
     );
