c9d3e9f91e54f0c9c8e459924b15008643dfb0dd
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-11-04 09:56:33 -0700

    Bring in day 2-4 forecast data to show city title, temperature range for the days, and conditions

--- src/Components/CustomList/CustomList.js
+++ src/Components/CustomList/CustomList.js
@@ -5,12 +5,41 @@ export class CustomList extends Component {
     const { weatherInfo } = this.props;
     return weatherInfo.map((city, i) => {
       return (
-        <div>
-          <h1>Future Outlook</h1>
+        <div className='forecast-card'>
           <h1>{city.title}</h1>
-          <h2>Day 2: {city.consolidated_weather[1].weather_state_name}</h2>
+          <h2>Day 2: {city.consolidated_weather[1].weather_state_name} </h2>
+          <h2>
+            Temperature (Degree Celsius):
+            <span className='max-temp'>
+              {city.consolidated_weather[1].max_temp}
+            </span>{' '}
+            /{' '}
+            <span className='min-temp'>
+              {city.consolidated_weather[1].min_temp}
+            </span>
+          </h2>
           <h2>Day 3: {city.consolidated_weather[2].weather_state_name}</h2>
+          <h2>
+            Temperature (Degree Celsius):
+            <span className='max-temp'>
+              {city.consolidated_weather[2].max_temp}
+            </span>{' '}
+            /{' '}
+            <span className='min-temp'>
+              {city.consolidated_weather[2].min_temp}
+            </span>
+          </h2>
           <h2>Day 4: {city.consolidated_weather[3].weather_state_name}</h2>
+          <h2>
+            Temperature (Degree Celsius):
+            <span className='max-temp'>
+              {city.consolidated_weather[3].max_temp}
+            </span>{' '}
+            /{' '}
+            <span className='min-temp'>
+              {city.consolidated_weather[3].min_temp}
+            </span>
+          </h2>
         </div>
       );
     });
