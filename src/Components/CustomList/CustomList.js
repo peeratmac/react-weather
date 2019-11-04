d63df57e57e893bd26f337b8e2cc52da7feafd17
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-11-04 09:55:42 -0700

    Initially bring in 3-day forecast to CustomList component

--- src/Components/CustomList/CustomList.js
+++ src/Components/CustomList/CustomList.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 export class CustomList extends Component {
   render() {
-    return (
-      <div>
-        <h1>Future Outlook</h1>
-      </div>
-    );
+    const { weatherInfo } = this.props;
+    return weatherInfo.map((city, i) => {
+      return (
+        <div>
+          <h1>Future Outlook</h1>
+          <h1>{city.title}</h1>
+          <h2>Day 2: {city.consolidated_weather[1].weather_state_name}</h2>
+          <h2>Day 3: {city.consolidated_weather[2].weather_state_name}</h2>
+          <h2>Day 4: {city.consolidated_weather[3].weather_state_name}</h2>
+        </div>
+      );
+    });
   }
 }
 
-export default CustomList;
+const mapStateToProps = state => ({
+  cities: state.cities,
+  stationIDs: state.stationIDs,
+  weatherInfo: state.weatherInformation
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(CustomList);
